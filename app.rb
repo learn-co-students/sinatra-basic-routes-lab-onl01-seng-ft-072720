94d8eeda2c3f66af2b25884bba0f5512dd24954e
NerlyTon <nerlyton@gmail.com>
2020-09-01 19:30:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Nerly Ton"
+    end
+
+    get '/hometown' do
+        "My hometown is Carrollton, TX"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Back At One by Brian Mcknight"
+    end
+
 end
