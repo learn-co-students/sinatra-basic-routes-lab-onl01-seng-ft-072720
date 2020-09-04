f6e3f056d84f6345e9303b0653be64b3afbaf86b
Evan Pavone <evanrpavone@gmail.com>
2020-09-04 15:38:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Evan"
+  end
+
+  get '/hometown' do
+    "My hometown is Fuquay-Varina"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Obey by Bring Me The Horizon"
+  end
 end
