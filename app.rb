df7dad541ac1c42ce333a30a3ca68117b7e3af85
Austin Cummings <aac729@live.com>
2020-09-07 21:08:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Austin"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Joliet"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Sad"
+  end
 end
