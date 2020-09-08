9cf957fc689a6d016e9d3d881ae7c527a993182a
VirgilCantu <cantu.sampsom@gmail.com>
2020-09-08 10:00:32 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Virgil Cantu"
+    end
+
+    get '/hometown' do
+        "My hometown is Miami, FL"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Kiburus by Leon Vynehall"
+    end
+
 end
